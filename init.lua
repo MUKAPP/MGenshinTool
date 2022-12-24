55e003eaf4c9538f234c068a7c57c72971d5521b
MUK <mukmiuikong@foxmail.com>
2022-12-24 21:35:45 +0800

    1.7.3

--- init.lua
+++ init.lua
@@ -1,29 +1,29 @@
-appname="应急食品"
-appver="1.7.2"
-appcode="17200"
-appsdk="21"
-path_pattern=""
-packagename="com.mukapp.genshintool"
-theme="Theme_Material_Light_NoActionBar"
-app_key="87e3062cb0"
-app_channel=""
-developer="mukmiuikong"
-description="MUKGenshinTool 应急食品"
-debugmode=true
-user_permission={
-  "ACCESS_COARSE_LOCATION",
-  "ACCESS_NETWORK_STATE",
-  "ACCESS_WIFI_STATE",
-  "DOWNLOAD_WITHOUT_NOTIFICATION",
-  "FOREGROUND_SERVICE",
-  "GET_PACKAGE_SIZE",
-  "INSTALL_SHORTCUT",
-  "INTERNET",
-  "READ_EXTERNAL_STORAGE",
-  "READ_LOGS",
-  "REQUEST_IGNORE_BATTERY_OPTIMIZATIONS",
-  "REQUEST_INSTALL_PACKAGES",
-  "SYSTEM_ALERT_WINDOW",
-  "WRITE_EXTERNAL_STORAGE",
-  "WRITE_SETTINGS"
+appname = "应急食品"
+appver = "1.7.3"
+appcode = "17300"
+appsdk = "21"
+path_pattern = ""
+packagename = "com.mukapp.genshintool"
+theme = "Theme_Material_Light_NoActionBar"
+app_key = "87e3062cb0"
+app_channel = ""
+developer = "mukmiuikong"
+description = "MUKGenshinTool 应急食品"
+debugmode = true
+user_permission = {
+    "ACCESS_COARSE_LOCATION",
+    "ACCESS_NETWORK_STATE",
+    "ACCESS_WIFI_STATE",
+    "DOWNLOAD_WITHOUT_NOTIFICATION",
+    "FOREGROUND_SERVICE",
+    "GET_PACKAGE_SIZE",
+    "INSTALL_SHORTCUT",
+    "INTERNET",
+    "READ_EXTERNAL_STORAGE",
+    "READ_LOGS",
+    "REQUEST_IGNORE_BATTERY_OPTIMIZATIONS",
+    "REQUEST_INSTALL_PACKAGES",
+    "SYSTEM_ALERT_WINDOW",
+    "WRITE_EXTERNAL_STORAGE",
+    "WRITE_SETTINGS"
 }
